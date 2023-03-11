58bf4d7fa8979849362f88f39266f91734f91e4f
Aditya Waskar <adityawaskar05@gmail.com>
2023-03-11 20:36:08 +0530

    Add admin page

--- Frontend/loc_react_supplychain/src/App.js
+++ Frontend/loc_react_supplychain/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Admin_component from "./components/admin/Admin_component";
 
 function App() {
-  return <div className="App">Hackethon</div>;
+  return (
+    <div className="App">
+      <Admin_component />
+    </div>
+  );
 }
 
 export default App;
